239fd0d6840b4901367eb1859d78a34fd6cb161d
Parsa33033 <parsa_h_m@outlook.com>
2023-04-04 15:34:06 +0200

    created the necessary packages, results created

---
+++ framework/src/main/java/org/example/result/Result.java
@@ -0,0 +1,58 @@
+package org.example.result;
+
+import java.util.Objects;
+import java.util.function.Function;
+import org.example.error.Error;
+
+public final class Result<O, E extends Error> {
+
+  private final O object;
+  private final E error;
+
+  private Result(O object, E error) {
+    this.object = object;
+    this.error = error;
+  }
+
+  public static <U, V extends Error> Result<U, V> of(U object, V error) {
+    if (error != null) {
+      return Result.error(error);
+    } else {
+      return Result.ok(object);
+    }
+  }
+
+  public static <U, V extends Error> Result<U, V> ok(U object) {
+    Objects.requireNonNull(object, "object should not be null");
+    return new Result<>(object, null);
+  }
+
+  public static <U, V extends Error> Result<U, V> error(V error) {
+    Objects.requireNonNull(error, "error should not be null");
+    return new Result<>(null, error);
+  }
+
+  public boolean isOk() {
+    return object != null;
+  }
+
+  public boolean isError() {
+    return error != null;
+  }
+
+  public <U> Result<U, E> map(Function<O, U> mapper) {
+    if (isOk()) {
+      return Result.ok(mapper.apply(object));
+    } else {
+      return Result.error(error);
+    }
+  }
+
+  public <V extends Error> Result<O, V> mapError(Function<E, V> mapper) {
+    if (isError()) {
+      return Result.error(mapper.apply(error));
+    } else {
+      return Result.ok(object);
+    }
+  }
+}
